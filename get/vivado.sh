18e6126b14e33db719df5c1ac90a58416f80e834
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2024-05-11 20:38:58 -0700

    chore: clean up get/vivado

--- get/vivado.sh
+++ get/vivado.sh
@@ -7,25 +7,28 @@ TMP_DIR="${HOME}/.Vivado"
 SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 CURRENT_DIR=$(pwd)
 
-# Require the devtools helpers
-if [ -z "${DEVTOOLS_DIR}" ]; then
+# Require devtools
+if [ -z "${DTC_EXITS}" ]; then
     echo "You must first source develop.sh"
     exit 1
+else
+    source ${DTC_EXISTS}/bash/helpers.sh
+    # Grab the latest host configuration
+    source ${HOME}/.devtools-config
 fi
 
-source ${DEVTOOLS_DIR}/bash/helpers.sh
-
-# We need a .devtools-config to describe the host configuration
-# By default, that's in the user's home directory
-source ${HOME}/.devtools-config
+# Make sure the variables we need were set in .devtools-config
 check_env DTC_XILINX_ROOT DTC_XILINX_VERSION
 
 # Borrow the user's home directory for Vivado to drop logs into
 mkdir -p ${TMP_DIR}
+# Tell Vivado to use it
+export TMP=${TMP_DIR}
 
 # Set up the Xilinx environment
-export XILINXD_LICENSE_FILE=${DTC_XILINX_LICENSE_PATH}
-export TMP=${TMP_DIR}
+if [[ -n ${DTC_XILINX_LICENSE_PATH} ]]; then
+    export XILINXD_LICENSE_FILE=${DTC_XILINX_LICENSE_PATH}
+fi
 
 # This can potentially hose this shell due to conflicting binaries
 # packaged with Vivado, so only use it for vivado
@@ -33,10 +36,5 @@ printf "!!!\r\n"
 printf "!!! Vivado tools are on PATH; only use this shell for Vivado\r\n"
 printf "!!!\r\n"
 
-# Grab the Xilinx paths
+# Grab the Xilinx tools
 source ${DTC_XILINX_ROOT}/Vivado/${DTC_XILINX_VERSION}/settings64.sh
-
-# If launched from devtools, get out of here before Vivado drops 100 logs
-if [[ "${CURRENT_DIR}" == "${DEVTOOLS_DIR}"* ]]; then
-    cd ${TMP_DIR}
-fi
