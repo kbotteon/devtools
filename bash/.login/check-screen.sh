faec94fb792668a8f2f8b6bee7c815d2ae11a6f5
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2024-11-02 13:40:37 -0700

    feat: check for tmux sessions too at login

--- bash/.login/check-screen.sh
+++ bash/.login/check-screen.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 ################################################################################
-# \brief Tests for active 'screen' and 'tmux' sessions at login, and displays a
-# banner if any exist
+# \brief Tests for active 'screen' and 'tmux' sessions at login
 ################################################################################
 
 # If screen is installed, check for any active sessions
@@ -22,5 +21,13 @@ fi
 
 # If tmux is installed, check for those too
 if [[ type tmux >/dev/null 2>&1 ]]; then
-
+    sessions=$(tmux list-sessions 2>/dev/null)
+    if [[ -n $sessions ]]; then
+        printf "\r\n"
+        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        echo "! YOU HAVE ACTIVE TMUX SESSIONS !"
+        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        printf "\r\n"
+        tmux list-sessions
+        printf "\r\n"
 fi
