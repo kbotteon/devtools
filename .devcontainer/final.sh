4cc9360673ad0088684a1c761a01045ebc57c6fd
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2025-03-23 06:20:00 +0000

    fix: drop git build from source

--- .devcontainer/final.sh
+++ .devcontainer/final.sh
@@ -3,12 +3,9 @@
 # \brief Post-creation setup to run as user specified in Dockerfile
 ################################################################################
 
-# Codespaces default
 WORKSPACE='/workspaces'
 THIS_REPO='devtools'
-
 TOOLS_DIR="${WORKSPACE}/tools"
-TMP_DIR="${WORKSPACE}/tmp"
 
 ################################################################################
 
@@ -21,11 +18,8 @@ ln -s /persist ${WORKSPACE}/${THIS_REPO}/.mounts/persist
 ################################################################################
 
 # These persist between start/stop/rebuild but not delete/create
-mkdir -p ${TMP_DIR}
-mkdir -p ${TOOLS_DIR}
-
-# Clone devtools so we can use its scripts
-cd ${TOOLS_DIR} && git clone https://github.com/kbotteon/devtools.git
+# mkdir -p ${TOOLS_DIR}
+# cd ${TOOLS_DIR} && git clone https://github.com/kbotteon/devtools.git
 
 ################################################################################
 
@@ -33,9 +27,3 @@ cd ${TOOLS_DIR} && git clone https://github.com/kbotteon/devtools.git
 mkdir -p ${HOME}/.vnc
 chmod 755 ${HOME}/.vnc
 ln -sf ${TOOLS_DIR}/devtools/vnc/xstatup-xfce ${HOME}/.vnc/xstartup
-
-# For the life of a Codespace, retain installed ssh keys and config
-# !!! Nope, this breaks key authentication, like with `gh cs ssh` so don't to it
-# mkdir -p ${TMP_DIR}/dot-ssh
-# hmod 700 ${TMP_DIR}/dot-ssh
-# ln -s ${TMP_DIR}/dot-ssh ${HOME}/.ssh
