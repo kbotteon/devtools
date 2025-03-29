c4260214f42257212c0824604235b6983eb29e5e
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2025-03-28 21:15:23 -0700

    feat: add a few more aliases

--- shell/my.sh
+++ shell/my.sh
@@ -6,6 +6,8 @@ fi
 # Filesystem info
 alias info-dir='LC_ALL=C LC_COLLATE=C ls -lhaF --group-directories-first'
 alias info-disk='du -hc --max-depth=1 .'
+alias ll='ls -lha --color=auto'
+alias ls='ls --color=auto'
 
 # Navigation
 alias ..="cd .. && ls"
@@ -14,3 +16,4 @@ alias ..="cd .. && ls"
 hgrep() {
     grep -e "$*" ${HISTFILE}
 }
+
