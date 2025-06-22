a0e39ff953b4407b0c83f573532775cbb45d647c
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2025-06-22 03:13:32 +0000

    feat: add a default ssh config

--- .devcontainer/final.sh
+++ .devcontainer/final.sh
@@ -6,7 +6,9 @@
 WS='/persist/sandboxes'
 PKG="${WS}/devtools"
 
-################################################################################
+#-------------------------------------------------------------------------------
+# Devtools
+#-------------------------------------------------------------------------------
 
 # Create symlinks for easy access
 mkdir -p ${WS}/.mounts && ln -sf /persist/home ${WS}/.mounts/home
@@ -14,14 +16,28 @@ mkdir -p ${WS}/.mounts && ln -sf /persist/home ${WS}/.mounts/home
 # This *is* the tools repo Codespace, so we don't need to clone it
 # cd ${WS} && git clone https://github.com/kbotteon/devtools.git
 
-################################################################################
+ln -sf ${PKG}/shell/develop.zsh ${HOME}/develop.ln
+touch ${HOME}/.zshrc && echo "source ${HOME}/develop.ln" >> ${HOME}/.zshrc
+
+#-------------------------------------------------------------------------------
+# VNC
+#-------------------------------------------------------------------------------
 
 # Set up the default VNC
 mkdir -p ${HOME}/.vnc
 chmod 755 ${HOME}/.vnc
 cp ${PKG}/vnc/xstartup-xfce ${HOME}/.vnc/xstartup
 
-################################################################################
+#-------------------------------------------------------------------------------
+# SSH Config
+#-------------------------------------------------------------------------------
 
-ln -sf ${PKG}/shell/develop.zsh ${HOME}/develop.ln
-touch ${HOME}/.zshrc && echo "source ${HOME}/develop.ln" >> ${HOME}/.zshrc
+mkdir -p ${HOME}/.ssh && chmod 700 ${HOME}/.ssh
+
+echo '# Access all private repos by default
+Host github.com
+    IdentityFile ~/.ssh/id_gh
+    User git
+' >> ${HOME}/.ssh/config
+
+#-------------------------------------------------------------------------------
