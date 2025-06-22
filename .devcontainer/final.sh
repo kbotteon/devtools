2ccb73f45eb8f401449e084a0dda68853fb7e6b4
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2025-06-22 01:56:35 +0000

    fix: get common work area with .mounts working

--- .devcontainer/final.sh
+++ .devcontainer/final.sh
@@ -3,35 +3,25 @@
 # \brief Post-creation setup to run as user specified in Dockerfile
 ################################################################################
 
-USER=$(whoami)
 WS='/sandboxes'
 PKG="${WS}/devtools"
-TOOLS="${WS}/tools"
 
 ################################################################################
 
 # Create symlinks for easy access
-mkdir ${WS}/.mounts && ln -s /persist ${PKG}/.mounts/persist
+mkdir -p ${WS}/.mounts && ln -sf /persist ${WS}/.mounts/persist
 
-# cd ${TOOLS_DIR} && git clone https://github.com/kbotteon/devtools.git
-ln -s ${WS}/devtools ${TOOLS}/devtools
-
-################################################################################
-
-# Migrate home directory to persistent storage
-mkdir /persist/home
-cp -a /home/${USER}/. /persist/home/
-rm -rf /home/${USER}
-ln -s /persist/home /home/${USER}
+# This *is* the tools repo Codespace, so we don't need to clone it
+# cd ${WS} && git clone https://github.com/kbotteon/devtools.git
 
 ################################################################################
 
 # Set up the default VNC
 mkdir -p ${HOME}/.vnc
 chmod 755 ${HOME}/.vnc
-cp ${TOOLS}/devtools/vnc/xstartup-xfce ${HOME}/.vnc/xstartup
+cp ${WS}/devtools/vnc/xstartup-xfce ${HOME}/.vnc/xstartup
 
 ################################################################################
 
-ln -s ${TOOLS}/devtools/shell/develop.sh ${HOME}/develop.ln
+ln -s ${WS}/devtools/shell/develop.zsh ${HOME}/develop.ln
 touch ${HOME}/.zshrc && echo "source ${HOME}/develop.ln" >> ${HOME}/.zshrc
