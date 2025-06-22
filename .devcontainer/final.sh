bf39d1aa76ad9fdd1fd498120be8890f4d9455f1
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2025-06-22 02:25:07 +0000

    fix: new folder hierarchy loose ends

--- .devcontainer/final.sh
+++ .devcontainer/final.sh
@@ -3,13 +3,13 @@
 # \brief Post-creation setup to run as user specified in Dockerfile
 ################################################################################
 
-WS='/sandboxes'
+WS='/persist/sandboxes'
 PKG="${WS}/devtools"
 
 ################################################################################
 
 # Create symlinks for easy access
-mkdir -p ${WS}/.mounts && ln -sf /persist ${WS}/.mounts/persist
+mkdir -p ${WS}/.mounts && ln -sf /persist/home ${WS}/.mounts/home
 
 # This *is* the tools repo Codespace, so we don't need to clone it
 # cd ${WS} && git clone https://github.com/kbotteon/devtools.git
@@ -19,9 +19,9 @@ mkdir -p ${WS}/.mounts && ln -sf /persist ${WS}/.mounts/persist
 # Set up the default VNC
 mkdir -p ${HOME}/.vnc
 chmod 755 ${HOME}/.vnc
-cp ${WS}/devtools/vnc/xstartup-xfce ${HOME}/.vnc/xstartup
+cp ${PKG}/vnc/xstartup-xfce ${HOME}/.vnc/xstartup
 
 ################################################################################
 
-ln -s ${WS}/devtools/shell/develop.zsh ${HOME}/develop.ln
+ln -sf ${PKG}/shell/develop.zsh ${HOME}/develop.ln
 touch ${HOME}/.zshrc && echo "source ${HOME}/develop.ln" >> ${HOME}/.zshrc
