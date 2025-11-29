08edf41771ea1df34d378c4c2311e313861d68fc
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2025-11-29 00:44:02 +0000

    fix: project.sh template

--- .devcontainer/project.sh
+++ .devcontainer/project.sh
@@ -9,5 +9,5 @@ RUN_AS_ROOT=1
 
 # Self-invoke as root if not already
 if [ "$EUID" -ne 0 ] && [ "$RUN_AS_ROOT" -ne 0 ]; then
-  exec sudo "$(realpath "$0")" "$@"
+  exec sudo /bin/bash "$(realpath "$0")" "$@"
 fi
